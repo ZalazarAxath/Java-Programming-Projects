262a948d6bffbbe00ca236d0f1ff514db85c750b
Luke <lukethomasyig@gmail.com>
2024-09-15 00:08:50 -0400

    Removed throws Exception

--- Excercise1_1.java
+++ Excercise1_1.java
@@ -1,6 +1,6 @@
 //DISPLAYTEXT Welcome to Java AND Welcome to Computer Science AND Programming is fun
 public class Excercise1_1 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
         System.out.println("Welcome to Java");
         System.out.println("Welcome to Computer Science");
         System.out.println("Programming is fun");
