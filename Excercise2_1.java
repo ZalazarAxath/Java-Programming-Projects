ea11aefd499e9afcc8a57eadbcfb97885e869e43
Luke <lukethomasyig@gmail.com>
2024-09-16 12:54:38 -0400

    Excercise 2.1 first draft

---
+++ Excercise2_1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Excercise2_1 {
+    public static void main (String args[])  
+    { float Fahrenheit, Celsius;  
+          Scanner Celsius= 13;  
+          Fahrenheit =((Celsius*9)/5)+32;  
+          System.out.println(Celsius + "Celsius is" + Fahrenheit ++ );  
+    }
+}
