a13adad09f9ce04e85ad977ce5a0bd7bb678922f
Luke <lukethomasyig@gmail.com>
2024-09-16 15:39:12 -0400

    Excercise 2.1 second draft

--- Excercise2_1.java
+++ Excercise2_1.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
 public class Excercise2_1 {
-    public static void main (String args[])  
-    { float Fahrenheit, Celsius;  
-          Scanner Celsius= 13;  
-          Fahrenheit =((Celsius*9)/5)+32;  
-          System.out.println(Celsius + "Celsius is" + Fahrenheit ++ );  
+    public static void main (String args[])  {  
+       Scanner tempCelsius = new Scanner(System.in);
+       System.out.println("Enter a degree in Celsius: ");
+       int celsius = tempCelsius.nextInt();
+       double fahrenheit = (celsius * 1.8) + 32;     
+       System.out.println(celsius + " Celsius is " + fahrenheit + (" Fahrenheit"));  
     }
 }
