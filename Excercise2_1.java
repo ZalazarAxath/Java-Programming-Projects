a8ed2a1bdd7d4bf8b6addab2ea3e8348a7062938
Luke <lukethomasyig@gmail.com>
2024-09-16 17:21:06 -0400

    Added comments

--- Excercise2_1.java
+++ Excercise2_1.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 
 public class Excercise2_1 {
     public static void main (String args[])  {  
+       //INITIALIZEINPUT
        Scanner tempCelsius = new Scanner(System.in);
+       //USERINPUT request tempCelsius 
        System.out.println("Enter a degree in Celsius: ");
+       //CALCULATEINPUT convert to fahrenheit
        int celsius = tempCelsius.nextInt();
        double fahrenheit = (celsius * 1.8) + 32;     
+       //DISPLAYTEXT output
        System.out.println(celsius + " Celsius is " + fahrenheit + (" Fahrenheit"));  
     }
 }
