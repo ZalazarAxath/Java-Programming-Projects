d20297ea0e6c8a1bedbdb393a45d096f619052e3
Luke <lukethomasyig@gmail.com>
2024-10-02 16:06:32 -0400

    Added Exercise 3.6

--- Exercise3_6.java
+++ Exercise3_6.java
@@ -29,9 +29,9 @@ public class Exercise3_6 {
         // Determine BMI category
         if (bmi < 18.5) {
             System.out.println("Underweight");
-        } else if (bmi >= 18.5 && bmi < 24.9) {
+        } else if (bmi >= 18.5 && bmi <= 24.9) {
             System.out.println("Normal");
-        } else if (bmi >= 25 && bmi < 29.9) {
+        } else if (bmi >= 25 && bmi <= 29.9) {
             System.out.println("Overweight");
         } else {
             System.out.println("Obese");
